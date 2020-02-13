73665643396172c9b789bc6dd02c5f66b500a52f
aaronfox <aaron.m.fox3@gmail.com>
2020-02-13 15:01:47 -0500

    Fixed encoding issue by changing UTF-8 encoding to latin1

--- Project 1/binary_vigenere_fox.py
+++ Project 1/binary_vigenere_fox.py
@@ -2,7 +2,7 @@
 # Project 1 (Binary Vigenere Crypto System)
 # CECS 564
 # Dr. Desoky
-# NOTE: The encoding here uses extended ASCII-256 encoding based on the first 256 characters UTF-8 encoding found at:
+# NOTE: The encoding here uses extended ASCII-256 encoding based on the first 256 characters latin1 (iso-8859-1) encoding found at:
 # this means that the following results from using the chr function, following the extended ASCII
 # table found here: https://www.ascii-code.com/
 # chr(252) == ü
@@ -16,17 +16,16 @@ import os
 import matplotlib.pyplot as plt
 
 # encrypt_vigenere encrypts the text in a given filepath and outputs it to the given filepath
-# NOTE: This uses the first 256 characters of UTF-8, which is equivalent 
+# NOTE: This uses the first 256 characters of latin1, which is equivalent 
 # INPUT: text_file_path (string): filepath of file containing extended ASCII text to be encrypted
 #        key (string): key that is to be used to encrypt the text
 #        output_file_name (string): just the file name of the output file to be placed in the current directory
-# OUTPUT: None. But saves encrypted .txt file to the current directory
+# OUTPUT: [unencrypted_text, encrypted_text] But saves encrypted .txt file to the current directory
 def encrypt_vigenere(text_file_path, key, output_file_name):
     print("Encrypting text using Vigenere encryption...")
     # Open ASCII text file for reading based on input path
-    text_file = open(text_file_path, 'r', encoding='utf-8') # windows-1252 is extended ascii 256
+    text_file = open(text_file_path, 'r', encoding='latin1')
     # Read all info of  extended ASCII characters only
-    # text = text_file.read()
     text = []
     i = 0
     while True:
@@ -45,14 +44,14 @@ def encrypt_vigenere(text_file_path, key, output_file_name):
     # m is the key length, as specified by the Project 1 prompt formula
     m = len(key)
 
+    encrypted_text= []
     # Encrypt file here
-    encrypted_file = open(output_file_name, "w", encoding="utf-8")
+    encrypted_file = open(output_file_name, "w", encoding="latin1")
     while index < len(text):
         # Yi = (Xi + Ki % m) % 256
         X_i = ord(text[index])
         K_i = ord(key[index % m])
         Y_i = (X_i + K_i) % 256
-        
 
         # Increment index each time to continue encrypting
         index = index + 1
@@ -64,10 +63,11 @@ def encrypt_vigenere(text_file_path, key, output_file_name):
         # print('K_i == ' + str(K_i))
         # print('Y_i == ' + str(Y_i))
         # print("chr(" + str(Y_i) + ") == " + chr(Y_i))
-        # encrypted_text.append(chr(Y_i))
+        encrypted_text.append(chr(Y_i))
 
     encrypted_file.close()
     print("Successfully encrypted text. It is stored in " + str(os.getcwd()) + "\\" + str(output_file_name))
+    return [''.join(text), ''.join(encrypted_text)]
 
 
 # Decrypt the text found in the filepath of filepath_of_encrypted based on the Vigenere Cipher
@@ -77,8 +77,8 @@ def encrypt_vigenere(text_file_path, key, output_file_name):
 # OUTPUT: None. But saves decrypted .txt file to the current directory
 def decrypt_vigenere(filepath_of_encrypted, key, output_file_name):
     print("Decrypting text using Vigenere process...")
-    encrypted_text = open(filepath_of_encrypted, 'r', encoding='utf-8')
-    decrypted_text_file = open(output_file_name, 'w', encoding='utf-8')
+    encrypted_text = open(filepath_of_encrypted, 'r', encoding='latin1')
+    decrypted_text_file = open(output_file_name, 'w', encoding='latin1')
     index = 0
     m = len(key)
     while True:
@@ -114,11 +114,24 @@ def graph_probability_of_each_character(probabilities_dict):
     plt.title('Probability of each character in a typical Text file')
     plt.show()
 
-# get_probability_density_of_text gets the probability density function of a typical text file
-# OUTPUT: probabilities_dict (dict): A dictionary containing the each extended ASCII character and the frequency of occuring
-def get_probability_density_of_text(filepath_of_text_to_get_pdf_from):
+
+# get_index_of_coincidence returns the index of coincence IOC) of a text
+def get_index_of_coincidence(text, count_of_each_letter_dict):
+    sum = 0
+    N = len(text)
+    for key in count_of_each_letter_dict.keys():
+        sum = sum + (count_of_each_letter_dict[key] * (count_of_each_letter_dict[key] - 1))
+
+    return sum / (N * (N - 1))
+
+
+# get_probability_dist_of_text gets the probability density function of a typical text file
+# OUTPUT: [probabilities_dict (dict), letter_count_dict (dict)]: An array containing a 
+# dictionary with each extended ASCII character and the probability of that character occuring and 
+# a dictionary with the count of each letter
+def get_probability_dist_of_text(filepath_of_text_to_get_pdf_from):
     print("Getting PDF of text...")
-    text_file = open(filepath_of_text_to_get_pdf_from, "r", encoding="utf-8")
+    text_file = open(filepath_of_text_to_get_pdf_from, "r", encoding="latin1")
 
     # Build out each character in Z_256 of probabilities dictionary with a count of 0 initially
     probabilities_dict = {}
@@ -138,11 +151,17 @@ def get_probability_density_of_text(filepath_of_text_to_get_pdf_from):
         else:
             print("Skipping character " + str(char) + ", because it is not in the extended ASCII table")
 
+    # Before getting probability, save it in the letter_count_dict
+    # for finding the index of coincidence of each letter
+    letter_count_dict = probabilities_dict.copy()
+
     # Get percentage of each characters usage
     for i in range(0, 256):
         probabilities_dict[chr(i)] = probabilities_dict[chr(i)] / total_number_of_characters
 
     graph_probability_of_each_character(probabilities_dict)
+
+    return [probabilities_dict, letter_count_dict]
     
            
 
@@ -153,16 +172,21 @@ def attack_vigenere_cipher(filepath_of_encrypted_file):
 if __name__== "__main__":
     # Encrypting file
     file_path = r"C:\Users\aaron\Classes_11th_Semester\CECS 564\CECS-564-Cryptography\Project 1\MansNotHot.txt"
-    # file_path = r"C:\Users\aaron\Classes_11th_Semester\CECS 564\CECS-564-Cryptography\Project 1\The_Lottery_Shirley_Jackson.txt"
+    file_path = r"C:\Users\aaron\Classes_11th_Semester\CECS 564\CECS-564-Cryptography\Project 1\The_Lottery_Shirley_Jackson.txt"
     encryption_key='pineapple'
-    encrypt_vigenere(text_file_path=file_path, key=encryption_key, output_file_name="fox_encrypted_vigenere_file.txt")
+    [unencrypted_text, encrypted_text] = encrypt_vigenere(text_file_path=file_path, key=encryption_key, output_file_name="fox_encrypted_vigenere_file.txt")
 
-    # Decrypting file
+    # # Decrypting file
     encrypted_file_path = r"C:\Users\aaron\Classes_11th_Semester\CECS 564\CECS-564-Cryptography\Project 1\fox_encrypted_vigenere_file.txt"
+    encrypted_file_path = r"C:\Users\aaron\Classes_11th_Semester\CECS 564\CECS-564-Cryptography\Project 1\encrypted.txt"
+    # encrypted_file_path = r"C:\Users\aaron\Classes_11th_Semester\CECS 564\CECS-564-Cryptography\Project 1\message_to_decrypt_from_teammate.txt"
+    encryption_key = "cthulhu"
     decrypt_vigenere(filepath_of_encrypted=encrypted_file_path, key=encryption_key, output_file_name="fox_decrypted_text.txt")
 
-    # Obtaining probability distribution of a typical text
-    typical_text = r"C:\Users\aaron\Classes_11th_Semester\CECS 564\CECS-564-Cryptography\Project 1\The_Lottery_Shirley_Jackson.txt"
-    get_probability_density_of_text(typical_text)
-    # Attacking encrypted file
+    # # Obtaining probability distribution of a typical text
+    # typical_text = r"C:\Users\aaron\Classes_11th_Semester\CECS 564\CECS-564-Cryptography\Project 1\The_Lottery_Shirley_Jackson.txt"
+    # [probabilities_dict, letter_count_dict] = get_probability_dist_of_text(typical_text)
+    # # Attacking encrypted file
+    # ioc = get_index_of_coincidence(unencrypted_text, letter_count_dict)
+    # print("ioc == " + str(ioc))
 
